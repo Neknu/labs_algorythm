56fb7163376a18a5c4ab3c40cb467013de1389cb
Daniel <d.tuytuyshkin@gmail.com>
2019-10-15 12:32:43 +0300

    add time

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -19,8 +19,17 @@ int main() {
 //    Matrix C = A * B;
 //    C.print();
 //    cout << endl;
+    float t;
+    using namespace std::chrono;
+    duration<double> time_span;
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
     Matrix C = A.StrassenMultiply(B);
+
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double>>(t2 - t1);
+    t = time_span.count();
+    cout << "It tooks me" <<t << "seconds";
 //    C.print();
     return 0;
 }
