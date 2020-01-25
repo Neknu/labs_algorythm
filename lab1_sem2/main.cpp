ce63d15d9bacbee6ad1c40ec3f849169c36965a6
Daniel <d.tuytuyshkin@gmail.com>
2020-01-25 11:21:21 +0200

    testing completed, command line shop is working, provide command line interface

--- lab1_sem2/main.cpp
+++ lab1_sem2/main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <ctime>
 #include <vector>
+#include <fstream>
 
 using std::string;
 using std::ifstream;
@@ -41,17 +42,19 @@ struct Node {
 
 vector<Product*> collect_data() {
     vector<Product*> products;
-    freopen("data.txt", "r", stdin);
+    ifstream f;
+    f.open("data.txt");
     string data;
-    while(cin >> data) {
+    while(f >> data) {
         auto product = new Product();
         product->name = data;
-        cin >> data;
+        f >> data;
         product->group = data;
-        cin >> data;
+        f >> data;
         product->cost = std::stoi(data);
         products.push_back(product);
     }
+    f.close();
     return products;
 }
 
@@ -59,7 +62,7 @@ int hash_product_name(string name, int count, int a = COEF, int b = 0, int mod =
     int hash = b;
     int length = name.length();
     for(int i = 0; i < length; i++) {
-        hash = (hash + ((int) name[i])*a) % mod;
+        hash = (hash + ((int) name[i])*a + b) % mod;
     }
     return hash % count;
 }
@@ -75,16 +78,22 @@ void create_inside_hash(Node* h) {
     }
     bool check_collissions = false;
     while(!check_collissions) {
-        check_collissions = true;
+        bool collissions[h->m];
+        for(int i = 0; i < h->m; i++) {
+            collissions[i] = false;
+        }
+
         h->a = rand_num( MOD - 2) + 1;
         h->b = rand_num( MOD - 1);
+        check_collissions = true;
         for(auto product:h->products) {
             int hash = hash_product_name(product->name, h->m, h->a, h->b);
-            if(h->hash_product[hash]) {
+            if(collissions[hash]) {
                 check_collissions = false;
                 break;
             } else {
                 h->hash_product[hash] = product;
+                collissions[hash] = true;
             }
         }
     }
@@ -103,15 +112,28 @@ vector<Node*> create_hash(const vector<Product*> &products) {
     }
 
     for(auto h:hash) {
-        create_inside_hash(h);
+        if(h->products.size() > 0) {
+            create_inside_hash(h);
+        }
     }
 
     return hash;
 }
 
+Product* getProductByName(vector<Node*> hash, const string &name) {
+    Node* h = hash[hash_product_name(name, hash.size())];
+    return h->hash_product[hash_product_name(name, h->m, h->a, h->b)];
+}
+
+void print_product(Product* product) {
+    cout << "name: " << product->name << " -- group: " << product->group << " -- cost: " << product->cost << endl;
+}
+
 void print_products(const vector<Product*> &products) {
+    cout << "WELCOME TO OUR MINIMARKET HASH:" << endl;
+    cout << "You can choose your product name:" << endl;
     for(auto product:products) {
-        cout << product->name << " " << product->group << " " << product->cost << " " << hash_product_name(product->name, products.size()) << endl;
+        print_product(product);
     }
 }
 
@@ -120,5 +142,9 @@ int main() {
     products = collect_data();
     print_products(products);
     vector<Node*> hash = create_hash(products);
+    cout << "Now please write what product you want " << endl;
+    string product_name;
+    cin >> product_name;
+    print_product(getProductByName(hash, product_name));
     return 0;
 }
